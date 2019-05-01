fba25e441dde442882b87b6263aa9672ddb75603
renefibonacci660 <660@holbertonschool.com>
2019-05-01 06:45:33 +0000

    Fixed output for numbers

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord(c) >= 122 and ord(c) <= 97):
-        return (False)
-    return (True)
+    if (ord(c) >= 97 and ord(c) <= 122):
+        return (True)
+    return (False)
