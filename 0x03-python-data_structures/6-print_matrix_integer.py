5fa0965bfe36789d7a510dc0807d8d442e657e1a
renefibonacci660 <660@holbertonschool.com>
2019-05-08 06:14:39 +0000

    One extra space but prints properly

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for index, i in enumerate(line):
-            print("{:d}".format(i), end=" ")
+            print("{:d}".format(i), end="")
             if (index == len(line)):
                 print("{}".format(" "), end="")
         print()
