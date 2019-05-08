eab58c4614733c036889154c8ed65d04c34ad173
renefibonacci660 <660@holbertonschool.com>
2019-05-08 06:27:41 +0000

    finds biggest int in list successfully

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        return(None)
+
+    maxNum = my_list[0]
+
+    for i in my_list:
+        if (maxNum < i):
+            maxNum = i
+    return (maxNum)
