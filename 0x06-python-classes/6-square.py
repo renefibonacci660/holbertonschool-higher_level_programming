2357b6f99a17f15e58495ee6e7803199d37712b5
renefibonacci660 <660@holbertonschool.com>
2019-05-22 04:08:00 +0000

    Now able to give correct error if tuple contains neg ints

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
 class Square():
-    """ A square class """
+    """A square class"""
+
     def __init__(self, size=0, position=(0, 0)):
         """ Initializing class """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
-    @property
-    def position(self):
-        """ property position """
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple) or not len(value) == 2:
-            if not isinstance(value[0], int) or not isinstance(value[1], int):
-                if value[0] <= 0 or value[1] <= 0:
-                    self.__position = value
-                    raise TypeError("position must be a tuple of 2 positive integers")
-                    return
-
+    def area(self):
+        """ Calculates area """
+        return self.__size * self.__size
 
     @property
     def size(self):
-        """ defining property size """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Defining property setter """
+        """ Property size setter """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """ Initializing area calc """
-        return self.__size * self.__size
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ Property position setter """
+        if isinstance(value, tuple) and len(value) == 2:
+                if isinstance(value[0], int) and isinstance(value[1], int):
+                    if value[0] >= 0 and value[1] >= 0:
+                        self.__position = value
+                        return
+        raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
-        """ Initializing printing square """
+        """ Prints square/tuple """
         if self.__size <= 0:
             print()
+            return
 
-        else:
-            print(((((" " * self.__position[0]) +
-                    ("#" * self.__size) + '\n')) * self.__size), end="")
+        print(((((" " * self.__position[0]) +
+                ("#" * self.__size) + '\n')) * self.__size), end="")
