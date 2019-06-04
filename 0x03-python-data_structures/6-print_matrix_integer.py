4f57fd54b1afeec83de582851eb934602dd23cbd
renefibonacci660 <660@holbertonschool.com>
2019-06-04 22:33:21 +0000

    Now prints proper spacing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for index, i in enumerate(line):
             print("{:d}".format(i), end="")
-            if (index == len(line)):
+            if (index < len(line) - 1):
                 print("{}".format(" "), end="")
         print()
