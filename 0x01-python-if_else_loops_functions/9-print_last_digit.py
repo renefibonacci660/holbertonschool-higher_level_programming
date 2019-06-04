5402be6213e1f124425eee56dfc3b0424ef61b8b
renefibonacci660 <660@holbertonschool.com>
2019-06-04 19:05:14 +0000

    Prints last digit and in correct styling

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = '{1d}'.format(number),
+
+    if number < 0:
+        number = number * -1
+    last = number % 10
+    print("{:d}".format(last), end="")
     return (last)
