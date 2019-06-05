f6e59d8517c7747336aa464cc83ed06b28a9447c
renefibonacci660 <660@holbertonschool.com>
2019-06-05 23:51:47 +0000

    raises exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+        Empty class
+'''
+class BaseGeometry():
+    '''
+        contains attribute area
+    '''
+    def area(self):
+        raise Exception('area() is not implemented')
