f910567d1322200af31a26afd941cfac645f446b
renefibonacci660 <660@holbertonschool.com>
2019-07-17 04:36:44 +0000

    Restructured id specifications

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-'id' INT UNIQUE AUTO_INCREMENT NOT NULL,
-PRIMARY KEY ('id'),
-name VARCHAR(256) NOT NULL,
-state_id INT NOT NULL,
-FOREIGN KEY('state_id') REFERENCES states('id'));
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       state_id INT NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id),
+       name VARCHAR(256) NOT NULL);
