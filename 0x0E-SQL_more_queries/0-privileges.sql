d059e043a37702beeb5e80a2e134726353272303
renefibonacci660 <660@holbertonschool.com>
2019-07-17 03:02:02 +0000

    Removed @localhost specification

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists privileges of users user_0d_1 & user_0d_2
-SHOW GRANTS FOR 'user_0d_1@localhost'@'localhost';
-SHOW GRANTS FOR 'user_0d_2@localhost'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
