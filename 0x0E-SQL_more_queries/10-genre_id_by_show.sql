0d5561ccd441d253bf75cf4cd7f55828d4977803
renefibonacci660 <660@holbertonschool.com>
2019-07-17 04:49:26 +0000

    lists tv shows with one genre in database

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in database with at least one genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
