4e263f0b9f5e707ec1e2e737f48fc011052e00e8
renefibonacci660 <660@holbertonschool.com>
2019-08-10 21:02:21 +0000

    Adding comments for clarity

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    '''
+        cursor.execute = executing the SQL query
+        cursor.execute = fetching all the results
+    '''
     connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                                  user=argv[1], passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
