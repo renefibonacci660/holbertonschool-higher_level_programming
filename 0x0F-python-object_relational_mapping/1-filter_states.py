566771a63af0d23923797021da98ebbd56d5a502
renefibonacci660 <660@holbertonschool.com>
2019-08-10 21:27:26 +0000

    Fixing styling error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                                  user=argv[1], passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'"
+                   "ORDER BY states.id ASC")
     rows = cursor.fetchall()
 
     for eachRow in rows:
