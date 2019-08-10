5f4d5a2eb0ea53a03d19153382524ef217d04acd
renefibonacci660 <660@holbertonschool.com>
2019-08-10 20:53:27 +0000

    Lists all states ordered from db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+    Lists all states from given database
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+
+    for eachRow in rows:
+        print(eachRow)
+
+    cursor.close()
+    connection.close()
