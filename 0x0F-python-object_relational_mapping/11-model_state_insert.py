24aea74e722f1c6f7a2c2af3b833fc68912754c3
renefibonacci660 <660@holbertonschool.com>
2019-08-12 02:30:32 +0000

    Adds new state obj to specified db

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+    Adds the State object “Louisiana” to the specified database
+'''
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(connection.format(user, passwd, db),
+                           pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)
+
+    State_to_add = State(name="Louisiana")
+    session().add(State_to_add)
+    session().commit()
+    obj = session().query(State).filter_by(name="Louisiana").first()
+    print(obj.id)
