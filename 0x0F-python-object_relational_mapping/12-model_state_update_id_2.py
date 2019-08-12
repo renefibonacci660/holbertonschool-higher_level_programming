c696678d7ed09579770a0a25991268c5082b1f84
renefibonacci660 <660@holbertonschool.com>
2019-08-12 06:08:47 +0000

    Changes name of state object

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+    changes the name of a State object from the database 
+'''
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(connection.format(user, passwd, db),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+
+    query = session().query(State).filter_by(id=2).first()
+    query.name = "New Mexico"
+    session().commit()
