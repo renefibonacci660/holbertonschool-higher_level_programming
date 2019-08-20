88f106e82da5f9f6bfe96d2771c3f44b06260ed1
renefibonacci660 <660@holbertonschool.com>
2019-08-20 23:20:39 +0000

    Changing format to using wc instead of content length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl -sI "$1" | grep Content-Length | cut -c -16
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
