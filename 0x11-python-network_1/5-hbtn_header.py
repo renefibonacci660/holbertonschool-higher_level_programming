fbd429347a3bb9b9e105734e15bbc1c0c8495363
renefibonacci660 <660@holbertonschool.com>
2019-08-26 21:58:18 +0000

    Using requests lib to display info in given URL header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Sends URL request
+# Displays "X-Request-Id" in the header
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
