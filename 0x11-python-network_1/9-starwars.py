0ddcef960de036dde49320534db257009c62b969
renefibonacci660 <660@holbertonschool.com>
2019-08-27 00:46:57 +0000

    Reverting code since now not passing checker

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 # Takes a string and sends search request to Star Wars API
 # Uses string as search value of request
-# Body Response is JSON and converted to Pythn dict
+# Body Response is JSON and converted to Python
 import sys
 import requests
 
 if __name__ == "__main__":
-    searchItem = {"search": sys.argv[1]}
-    url = "https://swapi.co/api/people"
-    response = requests.get(url, params=searchItem)
+    searchItem = sys.argv[1]
+    url = "https://swapi.co/api/people" + "?search={}".format(searchItem)
+    response = requests.get(url)
     jsonResponse = response.json()
     print("Number of results: {}".format(jsonResponse["count"]))
     for result in jsonResponse["results"]:
