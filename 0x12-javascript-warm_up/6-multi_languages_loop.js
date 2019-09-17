2fee423362de81cba23a430dc2b07478ec401af9
renefibonacci660 <660@holbertonschool.com>
2019-09-17 23:38:09 +0000

    Learning how to print elements of an array using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Printing an elements of an array using a loop
+const myLoveLangs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myLoveLangs.map((elements) => console.log(elements));
