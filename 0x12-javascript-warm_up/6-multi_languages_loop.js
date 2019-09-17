85d1c2efa5318511f2a3e2f0de9720bd600b55c9
renefibonacci660 <660@holbertonschool.com>
2019-09-17 23:44:25 +0000

    Truly using a loop now in C-like format

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 // Printing an elements of an array using a loop
 const myLoveLangs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-myLoveLangs.map((elements) => console.log(elements));
+for (let elements = 0; elements < 3; elements++) {
+  console.log('%s', myLoveLangs[elements]);
+}
