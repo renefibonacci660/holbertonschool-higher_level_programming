e4945303e397f209773c35a37dfd6271da5d29ab
renefibonacci660 <660@holbertonschool.com>
2019-09-18 04:15:33 +0000

    Computes and prints factorial with recursion

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Computes and prints a factorial
+function factorial (num) {
+  if (isNaN(parseInt(num))) {
+    return (1);
+  }
+  if (num === 1) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+console.log(factorial(parseInt(process.argv[2])));
