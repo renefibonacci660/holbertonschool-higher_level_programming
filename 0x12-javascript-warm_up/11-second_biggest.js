a8b27439421df8596795fcb8e68158de2645ac85
renefibonacci660 <660@holbertonschool.com>
2019-09-18 20:55:36 +0000

    Made a streamlined version of grabbing second biggest int using sort; this works for neg ints as well

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,8 @@
 #!/usr/bin/node
 // Searches the second biggest integer in the list of arguments
-function SecondBiggest (GivenArray) {
-  let Max = GivenArray[2];
-  let SecMax = GivenArray[3];
-
-  if (GivenArray.length === 2 || GivenArray.length === 3) {
-    return (0);
-  }
-
-  for (let index = 2; index < GivenArray.length; index++) {
-    if (GivenArray[index] > Max) {
-      SecMax = Max;
-      Max = GivenArray[index];
-    } else if (GivenArray[index] > SecMax && GivenArray[index] < Max) {
-      SecMax = GivenArray[index];
-    }
-  }
-  return (SecMax);
+const MyArray = process.argv.slice(2);
+if (!process.argv[2] || !process.argv[3]) {
+  console.log(0);
+} else {
+  console.log(MyArray.sort(function (a, b) { return b - a; })[1]);
 }
-console.log(SecondBiggest(process.argv));
