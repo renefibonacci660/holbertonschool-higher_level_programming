7532838489b71199ee5d19916c962860ce239025
renefibonacci660 <660@holbertonschool.com>
2019-09-23 19:39:55 +0000

    Defining a class with constructors

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Defining class 'Rectangle' with constructor and 2 declared attributes
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
