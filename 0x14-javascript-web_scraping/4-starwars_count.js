4770d5b387c0b24a50a2c8de7d54bd85850bf574
renefibonacci660 <660@holbertonschool.com>
2019-09-24 20:39:55 +0000

    Lists number of epsiodes actor is present in

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Prints num of movies where the character “Wedge Antilles” is present
+// Accomplished by parsing thru GET body and checking for character 18 under film/people/18
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let films = 0;
+    const Allfilms = JSON.parse(body).results;
+    for (let num = 0; num < Allfilms.length; num++) {
+      const characters = Allfilms[num].characters;
+      for (let i = 0; i < characters.length; i++) {
+        if (characters[i] === 'https://swapi.co/api/people/18/') {
+          films += 1;
+        }
+      }
+    }
+    console.log(films);
+  }
+});
