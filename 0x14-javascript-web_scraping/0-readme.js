f50379b053b012e297d413978b59c2fc1acf2e31
renefibonacci660 <660@holbertonschool.com>
2019-09-24 19:29:57 +0000

    Now printing err message properly

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@
 // Reading data in utf-8 format
 const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, data) => {
-  if (err) throw err;
+fs.readFile(process.argv[2], function (err, data) {
+  if (err) console.log(err);
   else console.log(data.toString());
 });
