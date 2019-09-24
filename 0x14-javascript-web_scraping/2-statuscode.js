126a1245d2acfc5aec517b0fcb7345ae0d514fe6
renefibonacci660 <660@holbertonschool.com>
2019-09-24 19:51:00 +0000

    Prints status code of get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Requests status code of a GET request and prints
+
+const request = require('request');
+
+request(process.argv[2], function (err, response) {
+  if (err) console.log(err);
+  else console.log('code: ' + response.statusCode);
+});
