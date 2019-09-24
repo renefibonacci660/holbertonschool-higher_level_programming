b098f1ea316045d5a5c51010b63200dca35c6625
renefibonacci660 <660@holbertonschool.com>
2019-09-24 18:45:27 +0000

    Reads file in command line and prints content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Reads and prints the content of a file
+// Requires fs module where readFile function is defined.
+// Reading data in utf-8 format
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
